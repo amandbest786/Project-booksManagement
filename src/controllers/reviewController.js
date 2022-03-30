53a13401c6f7e9c91c1c50c2ae132e249c15071c
amandbest786 <amandbest786@gmail.com>
2022-03-30 16:56:43 +0530

    project 3

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,55 +1,58 @@
 const bookModel = require("../models/bookModel.js");
 const reviewModel = require("../models/reviewModel.js");
 const moment = require("moment");
-
-// -----------validation-----------------------------------------------------------------------------------
-const isValidValue = function(value){   //it should not be like undefined or null.
-    if (typeof value === 'undefined' || value === null) return false   //if the value is undefined or null it will return false.
-    if (typeof value === 'string' && value.trim().length === 0) return false   //if the value is string & length is 0 it will return false.
-    return true
-}
-    
-const isValidDetails = function(details){   
-    return Object.keys(details).length > 0
-}
-
+const validator = require("../validator/validator.js")
 
 // -----------CreateReview-----------------------------------------------------------------------------------
 const createReview = async function(req, res) {
     try{
         const review = req.body 
+
         const bookId = req.params.bookId
+
         const {reviewedBy, rating} = review
+
         const isValidBookId = await bookModel.findOne({_id : bookId, isDeleted:false})       //finding the bookId
+
         if(!isValidBookId){
             return res.status(404).send({status:true, msg:"no book found."})      
-        }       
-        if(!isValidDetails(review)){
+        }
+
+        if(!validator.isValidDetails(review)){
             return res.status(400).send({status:false, msg:"please provide review of the book"})        //review is mandatory
         }
-        if(!isValidValue(reviewedBy)){
+
+        if(!validator.isValidValue(reviewedBy)){
             return res.status(400).send({status:false, msg:"please provide who reviewed the book"})     //reviewedBy is mandatory
         }
-        if(!isValidValue(rating)){
+
+        if(!validator.isValidValue(rating)){
             return res.status(400).send({status:false, msg:"please provide the rating"})        //rating is mandatory
         }
+
         if((rating<1) || (rating>5)){       //ratings should be in between 1 to 5.
             return res.status(400).send({status:false, msg:"rating should be greater than 1 and less than 5"})
         }
-        const release = moment().format("YYYY-MM-DD");      //Moment for record the date & time
-        const finalData = {
+
+        const release = moment().format("YYYY-MM-DD");     //Moment for record the date & time
+
+        const finalData = {                         //storing the data in the variable
             bookId : bookId,
-            ...review,          //storing the data in the variable
+            ...review,          
             reviewedAt : release
         }
+
         const data = await reviewModel.create(finalData)
+
         res.status(201).send({status:true, msg:"book review saved successfully",data:data})
         
         const reviewCount = await reviewModel.find({ bookId: bookId, isDeleted:false})
+
         await bookModel.findOneAndUpdate(
             { _id: bookId },
             { reviews: reviewCount.length }
         );
+
     }
     catch(err) {
         console.log(err)
@@ -62,27 +65,39 @@ const createReview = async function(req, res) {
 const updateReview = async function(req, res) {
     try{
         const bookId = req.params.bookId
+
         const IsValidBookId = await bookModel.findOne({_id : bookId, isDeleted:false})      //finding the bookId
+
         if (!IsValidBookId){
             return res.status(404).send({status:true, msg:"no book found to update review."})       //
         }
-        const reviewId = req.params.reviewId        
+
+        const reviewId = req.params.reviewId     
+
         const IsValidReviewId = await reviewModel.findOne({_id : reviewId, isDeleted:false})        //finding the reviewId
+
+        
         if (!IsValidReviewId){
             return res.status(400).send({status:true, msg:"no review exists to update."})
         }
+
         const dataToUpdate = req.body
-        if(!isValidDetails(dataToUpdate)){
+
+        if(!validator.isValidDetails(dataToUpdate)){
             res.status(400).send({status:false, msg:"Please provide the review details to update"})  //Validate the value that is provided by the Client.
         }
+
         const {review, rating, reviewedBy} = dataToUpdate
+
         if((rating<1) || (rating>5)){       //ratings should be in between 1 to 5.
             return res.status(400).send({status:false, msg:"rating should be greater than 1 and less than 5"})
         }
+
         const updatedDetails = await reviewModel.findOneAndUpdate(
             {_id : reviewId},    //finding the reviewId and update the details.
             {review : review, rating : rating, reviewedBy : reviewedBy},
             {new : true})
+
         res.status(201).send({status:true, msg:"review updated successfully", data:updatedDetails})
     }
     catch(err) {
@@ -96,21 +111,30 @@ const updateReview = async function(req, res) {
 const deleteReview = async function(req, res) {
     try{
         const bookId = req.params.bookId
+        
         const IsValidBookId = await bookModel.findOne({_id : bookId, isDeleted:false})
+        
         if (!IsValidBookId){
             return res.status(404).send({status:true, msg:"no book found to delete review."})
         }
+        
         const reviewId = req.params.reviewId
+        
         const IsValidReviewId = await reviewModel.findOne({_id : reviewId, isDeleted:false})
+        
         if (!IsValidReviewId){
             return res.status(404).send({status:true, msg:"no review exists to delete."})
         }
+        
         const deletedData = await reviewModel.findOneAndUpdate(
             {_id : reviewId},     //finding the reviewId and mark the isDeleted to true & update the date at deletedAt.
             {isDeleted : true, deletedAt : new Date()},
             {new : true})
+            
         res.status(201).send({status:true, msg:"review deleted successfully", data:deletedData})
+        
         const reviewCount = await reviewModel.find({ bookId: bookId,  isDeleted:false})
+        
         await bookModel.findOneAndUpdate(
             { _id: bookId },
             { reviews: reviewCount.length }
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,67 +1,65 @@
 const jwt = require('jsonwebtoken');
 const userModel = require("../models/userModel.js");
-
-
-// -----------validation-----------------------------------------------------------------------------------
-const isValidValue = function(value){   //it should not be like undefined or null.
-    if (typeof value === 'undefined' || value === null) return false   //if the value is undefined or null it will return false.
-    if (typeof value === 'string' && value.trim().length === 0) return false   //if the value is string & length is 0 it will return false.
-    return true
-}
-    
-const isValidDetails = function(details){   
-    return Object.keys(details).length > 0
-}
-
-const isValidTitle = function(title){       //enum handeling
-    return ['Mr', 'Mrs', 'Miss'].indexOf(title) !== -1
-}
+const validator = require("../validator/validator.js")
 
 // -----------CreateUser-----------------------------------------------------------------------------------
 const createUser = async function(req, res) {
     try{
         const user = req.body
-        if(!isValidDetails(user)){
+        if(!validator.isValidDetails(user)){
             res.status(400).send({status:false, msg:"Please provide the User details"})  //Validate the value that is provided by the Client.
         }
+
         const {title, name, phone, email, password} = user   
-        if (!isValidValue(title)){
+
+        if (!validator.isValidValue(title)){
             return res.status(400).send({status:false, msg:"Please provide the Title"})   //title is mandory 
         }
-        if (!isValidTitle(title)){
+
+        if (!validator.isValidTitle(title)){
             return res.status(400).send({status:false, msg:"Please should be Mr || Mrs || Miss"})   //Enum is mandory 
         }
-        if (!isValidValue(name)){
+
+        if (!validator.isValidValue(name)){
             return res.status(400).send({status:false, msg:"Please provide the Name"})   //name is mandory 
         }
-        if (!isValidValue(phone)){
+
+        if (!validator.isValidValue(phone)){
             return res.status(400).send({status:false, msg:"Please provide phone number"})    //phone is mandory
         }
+
         if(!/^(?:(?:\+|0{0,2})91(\s*[\ -]\s*)?|[0]?)?[789]\d{9}|(\d[ -]?){10}\d$/.test(phone)){
             return res.status(400).send({status:false,msg:"Please provide valid phone number"})    //Regex for checking the valid phone format
         }
+
         const phoneUsed = await userModel.findOne({phone})   //phone is unique
         if(phoneUsed){
             return res.status(400).send({status:false, msg:`${phone} is already exists`})   //checking the phone number is already exist or not.
         }    
-        if (!isValidValue(email)){
+
+        if (!validator.isValidValue(email)){
             return res.status(400).send({status:false, msg:"Please provide Email Address"})   //email is mandory
         }
+
         if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
             return res.status(400).send({status:false,msg:"Please provide valid Email Address"})    //Regex for checking the valid email format 
         }
+
         const emailUsed = await userModel.findOne({email})    //unique is email
         if(emailUsed){
             return res.status(400).send({status:false, msg:`${email} is already exists`})   //checking the email address is already exist or not.
         }
-        if (!isValidValue(password)){
+
+        if (!validator.isValidValue(password)){
             return res.status(400).send({status:false, msg:"Please provide the Password"})   //password is mandory 
         }
+
         if(!(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/.test(password))){
             return res.status(400).send({status:false,msg:"Please provide valid Password"})    //Regex for checking the valid password format 
         }
+
         const data = await userModel.create(user)  //creating the User details
-            res.status(201).send({status: true, msg : "User details saved successfully", data: data})
+        res.status(201).send({status: true, msg : "User details saved successfully", data: data})
     }
     catch(err) {
         console.log(err)
@@ -74,26 +72,33 @@ const createUser = async function(req, res) {
 const login = async function(req, res) {
     try{
         const login = req.body
-        if (!isValidDetails(login)){
+        if (!validator.isValidDetails(login)){
             return res.status(400).send({ status: false, msg: "Please provide the login Details" })   //validating the parameters of body
         }
+
         const {email, password} = login
-        if (!isValidValue(email)){
+
+        if (!validator.isValidValue(email)){
             return res.status(400).send({status:false, msg:"Please provide the Email Address"})   //email is mandatory
         }
-        if (!isValidValue(password)){
+
+        if (!validator.isValidValue(password)){
             return res.status(400).send({status:false, msg:"Please provide the password"})  //password is mandatory
         }
+
         const User = await userModel.findOne({email, password});   //validating the email/password in the userModel.
         if (!User){
         return res.status(401).send({status: false, msg: "Email or Password is not correct, Please check your credentials again.",})    
         }  
+
         const token = jwt.sign(   //creating the token for the authentication.
             {
                 userId : User._id   //payload(details that we saved in this token)
             },
                 "Project-Books", { expiresIn: '30000mins' });  //secret key with the expiry
+
         res.setHeader("x-api-key", token);  //setting token in header
+        
         res.status(200).send({ status: true, message: `User logged in successfully`, data: { token } });  
     }
     catch(err) {
